07c2c175132e21dcd8aae5e652764f0191e0d3ba
JIshanRandika <jishanrandika@gmail.com>
2022-01-13 07:41:59 +0530

    Home UI update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 
 @section('content')
-
+    <div class="container">
         <a class="dropdown-item" href={{url('mahapola')}} aria-expanded="false" v-pre>
             Mahapola
         </a>
         <a class="dropdown-item" href={{url('bursary')}} aria-expanded="false" v-pre>
             Bursary
         </a>
-
+    </div>
 
 
 
